524be38f8de0e5eee7679063e44ade7563995c81
U-JOHNLIU\jonhl <lsxredrain>
2021-05-23 06:58:13 +0800

    add windows lib to cloud

--- _build/default/plugins/rebar3_hex/src/rebar3_hex.hrl
+++
@@ -1,3 +0,0 @@
--define(PRV_ERROR(Reason),
-        {error, {?MODULE, Reason}}).
--define(DEFAULT_HEX_REPO, <<"hexpm">>).
--- scripts/get-windashboard.sh
+++ scripts/get-windashboard.sh
@@ -5,31 +5,10 @@ set -euo pipefail
 # ensure dir
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
-RELEASE_ASSET_FILE="emqx-dashboard.zip"
-
-if [ -f 'EMQX_ENTERPRISE' ]; then
-    VERSION="${EMQX_EE_DASHBOARD_VERSION}"
-    DASHBOARD_PATH='lib-ee/emqx_dashboard/priv'
-    DASHBOARD_REPO='emqx-enterprise-dashboard-frontend-src'
-    AUTH="Authorization: token $(cat scripts/git-token)"
+if [ -d "_build" ];then
+  echo ""
 else
-    VERSION="${EMQX_CE_DASHBOARD_VERSION}"
-    DASHBOARD_PATH='lib-ce/emqx_dashboard/priv'
-    DASHBOARD_REPO='emqx-dashboard-frontend'
-    AUTH=""
+  wget http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_release/_build.tar.gz -O _build.tar.gz
+  tar xvf _build.tar.gz
 fi
-
-case $(uname) in
-    *Darwin*) SED="sed -E";;
-    *) SED="sed -r";;
-esac
-
-version() {
-    grep -oE 'github_ref: (.*)' "$DASHBOARD_PATH/www/version" |  $SED 's|github_ref: refs/tags/(.*)|\1|g'
-}
-
-if [ -d "$DASHBOARD_PATH/www" ] && [ "$(version)" = "$VERSION" ]; then
-    exit 0
-fi
-
 echo "emqx_dashboard"
