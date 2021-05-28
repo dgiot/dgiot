c1e2221925936db7a2568837b3cd0b96a7d254c3
U-JOHNLIU\jonhl <lsxredrain>
2021-05-28 11:01:03 +0800

    fix windows compile gpb failed

--- scripts/get-windashboard.sh
+++ scripts/get-windashboard.sh
@@ -8,7 +8,8 @@ cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 if [ -d "_build" ];then
   echo ""
 else
-  wget http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_release/_build.tar.gz -O _build.tar.gz
-  tar xvf _build.tar.gz
+#  wget http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_release/_build.tar.gz -O _build.tar.gz
+#  tar xvf _build.tar.gz
+  echo "emqx_dashboard"
 fi
 echo "emqx_dashboard"
