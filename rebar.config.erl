aaac68ef4dbb0244deb7aa237ed916b1a30410bc
AvantLiu <avantliu@iotn2n.com>
2021-06-29 11:52:15 +0800

    delete sin

--- rebar.config.erl
+++ rebar.config.erl
@@ -197,7 +197,6 @@ overlay_vars_rel(RelType) ->
         , {enable_plugin_dgiot_topo, true}
         , {enable_plugin_dgiot_opc, true}
         , {enable_plugin_dgiot_niisten, true}
-        , {enable_plugin_dgiot_sinmahe, true}
         , {vm_args_file, VmArgs}
     ].
 
@@ -319,7 +318,6 @@ relx_plugin_apps_per_rel(cloud) ->
         , dgiot_topo
         , dgiot_opc
         , dgiot_niisten
-        , dgiot_sinmahe
     ];
 relx_plugin_apps_per_rel(edge) ->
     [].
