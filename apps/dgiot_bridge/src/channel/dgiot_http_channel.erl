045c2619b61b12129b886950ca2a905b58dc5e68
AvantLiu <avantliu@iotn2n.com>
2021-07-02 10:18:38 +0800

    ico,postdevice

--- apps/dgiot_bridge/src/channel/dgiot_http_channel.erl
+++ apps/dgiot_bridge/src/channel/dgiot_http_channel.erl
@@ -63,6 +63,20 @@
         description => #{
             zh => <<"路径"/utf8>>
         }
+    },
+    <<"ico">> => #{
+        order => 102,
+        type => string,
+        required => false,
+        default => <<"http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/shuwa_tech/zh/product/dgiot/channel/Http%E9%87%87%E9%9B%86%E5%9B%BE%E6%A0%87.png">>,
+        title => #{
+            en => <<"channel ICO">>,
+            zh => <<"通道ICO"/utf8>>
+        },
+        description => #{
+            en => <<"channel ICO">>,
+            zh => <<"通道ICO"/utf8>>
+        }
     }
 }).
 
