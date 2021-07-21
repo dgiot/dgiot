de0e935e91dfbbf83c385dc431a5f4eeca90c099
h7ml <h7ml@qq.com>
2021-07-21 21:54:19 +0800

    feat: 迁移脚本 && 字段修å值重复

--- apps/dgiot_device/src/device2.erl
+++ apps/dgiot_device/src/device2.erl
@@ -13,13 +13,54 @@
 -export([updata_data/0]).
 -include_lib("dgiot/include/logger.hrl").
 
-
+%%% @description 设备数据字段迁移
 updata_data() ->
-    Success = fun(Page) -> io:format("~p ~n", [Page])
-%%    ?LOG(error, "~p", [Page])
-%%                            dgiot_parse:update_object(<<"Device">>, DeviceId, #{})
-              end,
-    Success = fun(Page) -> io:format("~p ~n", [Page]) end,
+    Info = fun(Page) ->
+        case Page of
+            [#{<<"basedata">> := Basedata, <<"objectId">> := ObjectId, <<"devaddr">> := Devaddr}] ->
+                case Basedata of
+                    #{<<"basicdata">> := BasisData} ->
+                        PowerOnCtrl = maps:get(<<"PowerOnCtrl">>, BasisData, 0),
+                        PubCtrl = maps:get(<<"PubCtrl">>, BasisData, 1),
+                        PubFreq = maps:get(<<"PubFreq">>, BasisData, 20),
+                        ParaGet = maps:get(<<"ParaGet">>, BasisData, 1),
+                        FOTA = maps:get(<<"FOTA">>, BasisData, 0),
+                        AgreementRelease = maps:get(<<"AgreementRelease">>, BasisData, 0),
+                        PowerOffDelay = maps:get(<<"PowerOffDelay">>, BasisData, 48),
+                        ExpirationTime = maps:get(<<"expirationTime">>, Basedata, 0),
+                        CtrlSerialNo = maps:get(<<"CtrlSerialNo">>, BasisData, 0),
+                        MDSerialNo = maps:get(<<"MDSerialNo">>, BasisData, 0),
+                        CtrSoftVersion = maps:get(<<"CtrSoftVersion">>, BasisData, 0),
+                        ProtocolVersion = maps:get(<<"ProtocolVersion">>, BasisData, 0),
+                        PartAddr = maps:get(<<"partAddr">>, BasisData, 0),
+                        MDSoftVersion = maps:get(<<"MDSoftVersion">>, BasisData, 0),
+                        SIMSerialNo = maps:get(<<"SIMSerialNo">>, BasisData, 0),
+                        dgiot_parse:update_object(<<"Device">>, ObjectId, #{<<"profile">> => #{
+                            <<"PowerOnCtrl">> => PowerOnCtrl,
+                            <<"PubCtrl">> => PubCtrl,
+                            <<"PubFreq">> => PubFreq,
+                            <<"ParaGet">> => ParaGet,
+                            <<"FOTA">> => FOTA,
+                            <<"AgreementRelease">> => AgreementRelease,
+                            <<"PowerOffDelay">> => PowerOffDelay
+                        }, <<"form">> => #{
+                            <<"Devaddr">> => Devaddr,
+                            <<"ExpirationTime">> => ExpirationTime,
+                            <<"CtrlSerialNo">> => CtrlSerialNo,
+                            <<"MDSerialNo">> => MDSerialNo,
+                            <<"CtrSoftVersion">> => CtrSoftVersion,
+                            <<"ProtocolVersion">> => ProtocolVersion,
+                            <<"PartAddr">> => PartAddr,
+                            <<"MDSoftVersion">> => MDSoftVersion,
+                            <<"SIMSerialNo">> => SIMSerialNo
+                        }});
+                    _ ->
+                        pass
+                end;
+            R -> io:format("~p ~n", [R])
+        end
+                end,
+
     Query = #{<<"where">> => #{}},
-    dgiot_parse_loader:start(<<"Device">>, Query, 0, 100, 20000, Success),
+    dgiot_parse_loader:start(<<"Device">>, Query, 0, 1, 2000, Info),
     ok.
