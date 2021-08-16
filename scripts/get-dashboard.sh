e13984ff2c74351164c2782bf18171f4e5925701
lsxredrain <34489690@qq.com>
2021-08-16 15:39:59 +0800

    add emq_dashboard

--- scripts/get-dashboard.sh
+++ scripts/get-dashboard.sh
@@ -11,12 +11,12 @@ if [ -f 'EMQX_ENTERPRISE' ]; then
     VERSION="${EMQX_EE_DASHBOARD_VERSION}"
     DASHBOARD_PATH='lib-ee/emqx_dashboard/priv'
     DASHBOARD_REPO='emqx-dashboard-web'
-    DIRECT_DOWNLOAD_URL="https://github.com/emqx/${DASHBOARD_REPO}/releases/download/${VERSION}/${RELEASE_ASSET_FILE}"
+    DIRECT_DOWNLOAD_URL="http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_release/emqx_dashboard.zip"
 else
     VERSION="${EMQX_CE_DASHBOARD_VERSION}"
     DASHBOARD_PATH='lib-ce/emqx_dashboard/priv'
     DASHBOARD_REPO='emqx-dashboard-frontend'
-    DIRECT_DOWNLOAD_URL="https://github.com/emqx/${DASHBOARD_REPO}/releases/download/${VERSION}/${RELEASE_ASSET_FILE}"
+    DIRECT_DOWNLOAD_URL="http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_release/emqx_dashboard.zip"
 fi
 
 case $(uname) in
@@ -39,5 +39,5 @@ curl -L --silent --show-error \
 
 unzip -q "$RELEASE_ASSET_FILE" -d "$DASHBOARD_PATH"
 rm -rf "$DASHBOARD_PATH/www"
-mv "$DASHBOARD_PATH/dist" "$DASHBOARD_PATH/www"
+mv "$DASHBOARD_PATH/www" "$DASHBOARD_PATH/www"
 rm -f "$RELEASE_ASSET_FILE"
