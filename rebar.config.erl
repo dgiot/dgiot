007c9f2fe148ec9ecc79b3a98fcca0306603e9e6
lsxredrain <34489690@qq.com>
2021-09-04 16:00:53 +0800

    rm jieshun

--- rebar.config.erl
+++ rebar.config.erl
@@ -219,7 +219,6 @@ overlay_vars_rel(RelType) ->
         , {enable_plugin_dgiot_modbus, true}
         , {enable_plugin_dgiot_group, true}
         , {enable_plugin_dgiot_ffmpeg, true}
-        , {enable_plugin_dgiot_jieshun, true}
         , {vm_args_file, VmArgs}
     ].
 
@@ -348,7 +347,6 @@ relx_plugin_apps_per_rel(cloud) ->
         , dgiot_modbus
         , dgiot_group
         , dgiot_ffmpeg
-        , dgiot_jieshun
     ];
 relx_plugin_apps_per_rel(edge) ->
     [].
