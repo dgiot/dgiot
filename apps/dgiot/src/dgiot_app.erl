afe583ec0030168ca1a146997b7eb919e359e48c
lsxredrain <34489690@qq.com>
2021-09-09 16:07:06 +0800

    fix:

--- apps/dgiot/src/dgiot_app.erl
+++ apps/dgiot/src/dgiot_app.erl
@@ -32,6 +32,7 @@ start(_StartType, _StartArgs) ->
     {ok, Sup} = dgiot_sup:start_link(),
     start_plugin(Sup),
     dgiot_metrics:start(dgiot),
+    dgiot_pushgateway:start_link(),
     {ok, Sup}.
 
 stop(_State) ->
