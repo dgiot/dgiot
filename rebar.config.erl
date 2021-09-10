2e788fd34bf43edf4e5ffc4662344368170f7d06
AvantLiu <avantliu@iotn2n.com>
2021-09-10 18:32:33 +0800

    fix: device_card

--- rebar.config.erl
+++ rebar.config.erl
@@ -219,6 +219,7 @@ overlay_vars_rel(RelType) ->
         , {enable_plugin_dgiot_modbus, true}
         , {enable_plugin_dgiot_group, true}
         , {enable_plugin_dgiot_ffmpeg, true}
+        , {enable_plugin_dgiot_shouyincheng, true}
         , {vm_args_file, VmArgs}
     ].
 
@@ -347,6 +348,7 @@ relx_plugin_apps_per_rel(cloud) ->
         , dgiot_modbus
         , dgiot_group
         , dgiot_ffmpeg
+        , dgiot_shouyincheng
     ];
 relx_plugin_apps_per_rel(edge) ->
     [].
