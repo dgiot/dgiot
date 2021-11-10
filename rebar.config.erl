e9feb9e12ff8bcf2496cac64bae3c0343b8e1b34
AvantLiu <47805007+AvantLiu@users.noreply.github.com>
2021-11-10 11:46:26 +0800

    Update rebar.config.erl

--- rebar.config.erl
+++ rebar.config.erl
@@ -221,7 +221,6 @@ overlay_vars_rel(RelType) ->
         , {enable_plugin_dgiot_ffmpeg, true}
         , {enable_plugin_dgiot_shouyincheng, true}
         , {enable_plugin_dgiot_gb26875, true}
-        , {enable_plugin_dgiot_esports, true}
         , {vm_args_file, VmArgs}
     ].
 
@@ -352,8 +351,7 @@ relx_plugin_apps_per_rel(cloud) ->
         , dgiot_group
         , dgiot_ffmpeg
         , dgiot_shouyincheng
-        , dgiot_gb26875
-        , dgiot_esports
+        , dgiot_gb26875    
     ];
 relx_plugin_apps_per_rel(edge) ->
     [].
