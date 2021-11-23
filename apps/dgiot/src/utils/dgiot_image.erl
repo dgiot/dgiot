24e2bb117682c92de78c89ea77a0681c8e526a7a
lsxredrain <34489690@qq.com>
2021-11-23 12:29:28 +0800

    feat: add dgiot_image

--- apps/dgiot/src/utils/dgiot_image.erl
+++ apps/dgiot/src/utils/dgiot_image.erl
@@ -1,11 +1,18 @@
-%%%-------------------------------------------------------------------
-%%% @author jonhl
-%%% @copyright (C) 2021, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 23. 11月 2021 12:14
-%%%-------------------------------------------------------------------
+%%--------------------------------------------------------------------
+%% Copyright (c) 2020-2021 DGIOT Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
 -module(dgiot_image).
 -author("jonhl").
 
