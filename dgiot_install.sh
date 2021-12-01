03f348da02fc5253a7b656b0cdb5ddcd654005e5
dawnwinterLiu <47805007+dawnwinterLiu@users.noreply.github.com>
2021-12-01 14:43:26 +0800

    dgiot_89

--- dgiot_install.sh
+++ dgiot_install.sh
@@ -110,7 +110,7 @@ fi
 # set parameters by default value
 verType=single        # [single | cluster]
 domain_name="prod.iotn2n.com" #[prod.iotn2n.com | your_domain_name]
-dgiot="dgiot_87"  #[dgiot_87 | dgiot_n]
+dgiot="dgiot_89"  #[dgiot_89 | dgiot_n]
 while getopts "h:v:d:s:" arg
 do
   case $arg in
