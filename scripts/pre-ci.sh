5666c8a98da4e1ca79630fe2c7c92fc9cc70d442
U-JOHNLIU\jonhl <johnliu>
2022-01-07 15:12:59 +0800

    fix: add make ci

--- scripts/pre-ci.sh
+++ scripts/pre-ci.sh
@@ -20,47 +20,13 @@ get_dashboard(){
     rm www.tar.gz -rf
     chmod 777 ./scripts/*
   fi
-
-  if [ -d "$DGIOT_DASHBOARD_PATH/www" ]; then
-      echo "dgiot_dashboard exist"
-  else
-      git clone -b gh-pages https://gitee.com/dgiiot/dgiot-dashboard.git dist
-      rm dist/.git/ -rf
-      mv dist  "$DGIOT_DASHBOARD_PATH/www"
-
-      ## dgiot_file
-      rm dgiot_file.tar.gz -rf
-      wget ${FILESEVER}/dgiot_file.tar.gz
-      rm dgiot_file/ -rf
-      tar xvf dgiot_file.tar.gz
-      rm dgiot_file.tar.gz -rf
-      mv  dgiot_file "$DGIOT_DASHBOARD_PATH/www/"
-
-      ## swagger文件
-      rm dgiot_swagger.tar.gz -rf
-      wget ${FILESEVER}/dgiot_swagger.tar.gz
-      rm dgiot_swagger/ -rf
-      tar xvf dgiot_swagger.tar.gz
-      rm dgiot_swagger.tar.gz -rf
-      mv dgiot_swagger "$DGIOT_DASHBOARD_PATH/www/"
-
-      ## dgiot_dashboard静态文件
-      rm dgiot_dashboard.tar.gz -rf
-      wget ${FILESEVER}/dgiot_dashboard.tar.gz
-      rm dgiot_dashboard/ -rf
-      tar xvf dgiot_dashboard.tar.gz
-      rm dgiot_dashboard.tar.gz -rf
-      mv  dgiot_dashboard "$DGIOT_DASHBOARD_PATH/www/"
-  fi
 }
 
 get_apps() {
     cd ${EMQX_APP}
-
     if [ ! -d "emqx_rule_engine/" ]; then
       git clone  -b "v4.3.10" "https://gitee.com/fastdgiot/emqx-rule-engine.git"  emqx_rule_engine
     fi
-
     if [ ! -d "emqx_bridge_mqtt/" ]; then
       git clone  -b "v4.3.10" "https://gitee.com/fastdgiot/emqx-bridge-mqtt.git"  emqx_bridge_mqtt
     fi
@@ -128,6 +94,6 @@ get_apps() {
     cd ..
 }
 
-
+ echo "ci"
  get_apps
  get_dashboard
