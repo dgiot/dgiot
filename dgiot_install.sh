b38c5f457d756c457a0ca883d351bdb4609451bc
U-JOHNLIU\jonhl <johnliu>
2022-02-11 09:29:24 +0800

    fix: ci failed change dist to www

--- dgiot_install.sh
+++ dgiot_install.sh
@@ -1196,12 +1196,11 @@ function pre_build_dgiot() {
     git reset --hard
     git pull
 
-    rm ${script_dir}/dgiot/apps/dgiot_api/priv/www/ -rf
     rm ${script_dir}/dgiot/_build/emqx/rel/ -rf
     rm ${script_dir}/dgiot/emqx/lib/dgiot_api/ -rf
-    mkdir -p ${script_dir}/dgiot/apps/dgiot_api/priv/www/
 
     if [ -d ${script_dir}/dgiot_dashboard/dist ]; then
+      rm ${script_dir}/dgiot/apps/dgiot_api/priv/www/ -rf
       cp ${script_dir}/dgiot_dashboard/dist/ ${script_dir}/dgiot/apps/dgiot_api/priv/www/ -rf
     fi
 
