30d59cf6f6b98e555411a877bb9f578ff112f015
U-JOHNLIU\jonhl <johnliu>
2022-02-25 17:43:17 +0800

    feat: add dgiot_amis

---
+++ apps/dgiot_amis/src/dgiot_amis_channel.erl
@@ -0,0 +1,130 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2020 DGIOT Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+
+-module(dgiot_amis_channel).
+-behavior(dgiot_channelx).
+-author("johnliu").
+-include_lib("dgiot_bridge/include/dgiot_bridge.hrl").
+-include_lib("dgiot/include/dgiot_socket.hrl").
+-include_lib("dgiot/include/logger.hrl").
+-include("dgiot_amis.hrl").
+-define(TYPE, <<"AMIS">>).
+-define(MAX_BUFF_SIZE, 1024).
+
+%% API
+-export([start/2]).
+
+%% Channel callback
+-export([init/3, handle_init/1, handle_event/3, handle_message/2, stop/3]).
+
+%% 注册通道类型
+-channel_type(#{
+    cType => ?TYPE,
+    type => ?BRIDGE_CHL,
+    title => #{
+        zh => <<"AMIS资源通道"/utf8>>
+    },
+    description => #{
+        zh => <<"AMIS资源通道"/utf8>>
+    }
+}).
+%% 注册通道参数
+-params(#{
+    <<"port">> => #{
+        order => 1,
+        type => integer,
+        required => true,
+        default => 61888,
+        title => #{
+            zh => <<"端口"/utf8>>
+        },
+        description => #{
+            zh => <<"侦听端口"/utf8>>
+        }
+    },
+    <<"ico">> => #{
+        order => 102,
+        type => string,
+        required => false,
+        default => <<"http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/shuwa_tech/zh/product/dgiot/channel/amis.jpg">>,
+        title => #{
+            en => <<"channel ICO">>,
+            zh => <<"通道ICO"/utf8>>
+        },
+        description => #{
+            en => <<"channel ICO">>,
+            zh => <<"通道ICO"/utf8>>
+        }
+    }
+}).
+
+
+start(ChannelId, ChannelArgs) ->
+    dgiot_channelx:add(?TYPE, ChannelId, ?MODULE, ChannelArgs).
+
+%% 通道初始化
+init(?TYPE, ChannelId, #{
+    <<"product">> := Products,
+    <<"search">> := Search}) ->
+    lists:map(fun(X) ->
+        case X of
+            {ProductId, #{<<"ACL">> := Acl, <<"nodeType">> := 1,<<"thing">> := Thing}} ->
+                dgiot_data:insert({amis, ChannelId}, {ProductId, Acl, maps:get(<<"properties">>,Thing,[])});
+            _ ->
+                ?LOG(info,"X ~p", [X]),
+                pass
+        end
+              end, Products),
+    dgiot_data:set_consumer(ChannelId, 20),
+    State = #state{
+        id = ChannelId,
+        search = Search
+    },
+    {ok, State, []};
+
+init(?TYPE, _ChannelId, _Args) ->
+    {ok, #{}, #{}}.
+
+handle_init(State) ->
+    {ok, State}.
+
+%% 通道消息处理,注意：进程池调用
+handle_event(_EventId, _Event, State) ->
+    {ok, State}.
+
+% SELECT clientid, payload, topic FROM "meter"
+% SELECT clientid, disconnected_at FROM "$events/client_disconnected" WHERE username = 'dgiot'
+% SELECT clientid, connected_at FROM "$events/client_connected" WHERE username = 'dgiot'
+handle_message({rule, #{clientid := DtuAddr, connected_at := _ConnectedAt}, #{peername := PeerName} = _Context}, #state{id = ChannelId} = State) ->
+    ?LOG(error,"DtuAddr ~p PeerName ~p",[DtuAddr,PeerName] ),
+    DTUIP = dgiot_utils:get_ip(PeerName),
+    dgiot_amis:create_amis(DtuAddr, ChannelId, DTUIP),
+    {ok, State};
+
+handle_message({rule, #{clientid := DevAddr, disconnected_at := _DisconnectedAt}, _Context}, State) ->
+    ?LOG(error,"DevAddr ~p ",[DevAddr] ),
+    {ok, State};
+
+handle_message({rule, #{clientid := DevAddr, payload := Payload, topic := _Topic}, _Msg}, #state{id = ChannelId} = State) ->
+    ?LOG(error,"DevAddr ~p Payload ~p ChannelId ~p",[DevAddr,Payload,ChannelId] ),
+    {ok, State};
+
+handle_message(_Message, State) ->
+    {ok, State}.
+
+stop(_ChannelType, _ChannelId, _State) ->
+    ok.
