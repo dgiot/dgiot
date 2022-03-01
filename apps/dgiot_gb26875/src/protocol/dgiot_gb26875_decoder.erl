0168f4eb4ab46297a419f4e82eda7c4ebc3de830
U-JOHNLIU\jonhl <johnliu>
2022-03-01 20:13:57 +0800

    fix: add gb26875 proctol

--- apps/dgiot_gb26875/src/protocol/dgiot_gb26875_decoder.erl
+++ apps/dgiot_gb26875/src/protocol/dgiot_gb26875_decoder.erl
@@ -1,14 +1,22 @@
-%%%-------------------------------------------------------------------
-%%% @author weixingzheng
-%%% @copyright (C) 2018, <COMPANY>
-%%% @doc
-%%% 南网上行通信规约
-%%% @end
-%%% Created : 08. 十一月 2018 14:49
-%%%-------------------------------------------------------------------
+%%--------------------------------------------------------------------
+%% Copyright (c) 2020-2021 DGIOT Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
 -module(dgiot_gb26875_decoder).
 -include_lib("dgiot_gb26875.hrl").
--author("weixingzheng").
+-author("stoneliu").
 -include_lib("dgiot/include/logger.hrl").
 -protocol([?GB26875]).
 
