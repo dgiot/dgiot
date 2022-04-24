142908cfd36ff00b7b54afaeaed9d30fc85c8b1f
dgiot <dgiot@foxmail.com>
2022-04-24 11:13:19 +0800

    api: Adaptive Amis

--- apps/dgiot_bamis/src/dgiot_bamis.erl
+++ apps/dgiot_bamis/src/dgiot_bamis.erl
@@ -33,27 +33,59 @@
 
 -define(APP, ?MODULE).
 %% amis api 接口适配
-get({'before', Data})  ->
+%% https://aisuda.bce.baidu.com/amis/zh-CN/docs/types/api#%E6%8E%A5%E5%8F%A3%E8%BF%94%E5%9B%9E%E6%A0%BC%E5%BC%8F-%E9%87%8D%E8%A6%81-
+
+%% @description 接口说明
+%% before 请求前的接口适配器拦截
+%% after 请求返回的数据适配器，目前已适配amis返回格式参数，参数如下
+
+%% @description 参数说明
+%% status: 返回 0，表示当前接口正确返回，否则按错误请求处理；
+%% msg: 返回接口处理信息，主要用于表单提交或请求失败时的 toast 显示；
+%% data: 必须返回一个具有 key-value 结构的对象。
+
+%% @description 备注
+%% 已实现 get post 的适配工作
+get({'before', Data}) ->
+    io:format("amis get before: ~p~n", [Data]),
     Data;
-get({'after', #{<<"results">> := Data}}) ->
-    #{<<"data">> => Data};
 get({'after', Data}) ->
-    #{<<"data">> => Data}.
-
+    io:format("amis get after: ~p~n", [Data]),
+    #{
+        <<"status">> => 0,
+        <<"msg">> => <<"数据请求成功"/utf8>>,
+        <<"data">> => Data
+    }.
 post({'before', Data}) ->
+    io:format("amis post before ~p~n", [Data]),
     Data;
-post({'after',Data}) ->
-    #{<<"data">> => Data}.
-
-put({'before',Data}) ->
+post({'after', Data}) ->
+    io:format("amis post after ~p~n", [Data]),
+    #{
+        <<"status">> => 0,
+        <<"msg">> => <<"数据提交成功"/utf8>>,
+        <<"data">> => Data
+    }.
+put({'before', Data}) ->
+    io:format("amis put before ~p~n", [Data]),
     Data;
-put({'after',Data}) ->
-    #{<<"data">> => Data}.
-
+put({'after', Data}) ->
+    io:format("amis put after ~p~n", [Data]),
+    #{
+        <<"status">> => 0,
+        <<"msg">> => <<"修改成功"/utf8>>,
+        <<"data">> => Data
+    }.
 delete({'before', Data}) ->
+    io:format("amis delete before ~p~n", [Data]),
     Data;
 delete({'after', Data}) ->
-    #{<<"data">> => Data}.
+    io:format("amis delete after ~p~n", [Data]),
+    #{
+        <<"status">> => 0,
+        <<"msg">> => <<"删除成功"/utf8>>,
+        <<"data">> => Data
+    }.
 
 del_amis_device(DeviceId) ->
     dgiot_device:delete(DeviceId).
