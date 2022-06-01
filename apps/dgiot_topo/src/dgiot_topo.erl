f350d2a31de1dd9e17e942280a4a9765a859b768
dawnwinterLiu <1737801684@qq.com>
2022-06-01 10:32:24 +0800

    feat: topic add /user;modbus env = <<>>

--- apps/dgiot_topo/src/dgiot_topo.erl
+++ apps/dgiot_topo/src/dgiot_topo.erl
@@ -81,7 +81,7 @@ get_name(ProductId, K, V) ->
 %% 发送实时卡片数据
 send_realtime_card(ProductId, DeviceId, Payload) ->
     Data = dgiot_device_card:get_card(ProductId, [Payload], DeviceId, #{}),
-    Pubtopic = <<"$dg/user/", DeviceId/binary, "/realtimecard/report">>,
+    Pubtopic = <<"$dg/user/realtimecard/", DeviceId/binary, "/report">>,
     dgiot_mqtt:publish(self(), Pubtopic, base64:encode(jsx:encode(#{<<"data">> => Data}))).
 
 %% 发送实时组态数据
