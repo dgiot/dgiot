19dc030c734232931393126fae8a88081a9c7170
Evan You <h7ml@qq.com>
2022-06-01 11:59:30 +0800

    fix: delete no use code

--- apps/dgiot_task/src/dgiot_task.erl
+++ apps/dgiot_task/src/dgiot_task.erl
@@ -292,13 +292,11 @@ save_td(ProductId, DevAddr, Ack, AppData) ->
             Interval = maps:get(<<"interval">>, AppData, 3),
             AllData = merge_cache_data(DeviceId, NewData, Interval),
             AllDataKey = maps:keys(AllData),
-            Flag = Keys -- AllDataKey,
             case Keys -- AllDataKey of
                 List when length(List) == 0 andalso length(AllDataKey) =/= 0 ->
                     ChannelId = dgiot_parse_id:get_channelid(dgiot_utils:to_binary(?BRIDGE_CHL), <<"DGIOTTOPO">>, <<"TOPO组态通道"/utf8>>),
                     dgiot_channelx:do_message(ChannelId, {topo_thing, ProductId, DeviceId, AllData}),
                     dgiot_tdengine_adapter:save(ProductId, DevAddr, AllData),
- 
                     Channel = dgiot_product:get_taskchannel(ProductId),
                     dgiot_bridge:send_log(Channel, ProductId, DevAddr, "~s ~p save td => ProductId ~p DevAddr ~p ~ts ", [?FILE, ?LINE, ProductId, DevAddr, unicode:characters_to_list(jsx:encode(AllData))]),
                     dgiot_metrics:inc(dgiot_task, <<"task_save">>, 1),
