3ba003688c4ad67d10d87c734979e46012f894bf
dawnwinterLiu <1737801684@qq.com>
2022-06-30 12:26:52 +0800

    fix: dlink_mqttc remove zeta

--- apps/dgiot_dlink/src/mock/dlink_mqttc.erl
+++ apps/dgiot_dlink/src/mock/dlink_mqttc.erl
@@ -18,15 +18,13 @@
 -include_lib("dgiot/include/logger.hrl").
 -include_lib("dgiot/include/dgiot_client.hrl").
 
--record(zeta, {tid}).
-
 -export([childspec/2, start_client/2]).
 
 %% API
 -export([init/1, handle_cast/2, handle_call/3, handle_info/2, terminate/2, code_change/3]).
 
 childspec(ChannelId, #{<<"host">> := Host, <<"port">> := Port,
-    <<"username">> := UserName, <<"password">> :=  Password}) ->
+    <<"username">> := UserName, <<"password">> := Password}) ->
     Options = #{
         host => dgiot_utils:to_list(Host),
         port => Port,
@@ -55,7 +53,7 @@ start_client(ChannelId, Count) ->
 %%  callback
 init(#dclient{channel = ChannelId} = Dclient) ->
     <<_:11/binary, Tid/binary>> = dgiot_utils:to_binary(ChannelId),
-    User = #zeta{tid = dgiot_utils:to_atom(Tid)},
+    User = #{tid => dgiot_utils:to_atom(Tid)},
     {ok, Dclient#dclient{channel = ChannelId, child = User}}.
 
 handle_call(_Request, _From, State) ->
@@ -66,34 +64,17 @@ handle_cast(_Request, State) ->
 
 handle_info({connect, Client}, #dclient{channel = ChannelId} = Dclient) ->
     erlang:send_after(2000, self(), {subscribe, Client}),
-    dgiot_metrics:inc(dgiot_zeta, <<"mqtt_online">>, 1),
     update(ChannelId),
     {noreply, Dclient};
 
-handle_info({subscribe, Client}, #dclient{client = ClientId, child = #zeta{tid = Tid}} = Dclient) ->
-    io:format("~s ~p Tid: ~p Client ~p ~n", [?FILE, ?LINE, Tid, Client]),
-    #{<<"apiKey">> := Key} = zeta_config:get_config(Tid, <<"zeta_auth">>),
-    case re:split(ClientId, <<":">>) of
-        [_, _, <<"001">>] ->
-            Topic = <<Key/binary, "/v1/ms/#">>,
-            ?LOG(info, "Topic ~p ", [Topic]),
-            emqtt:subscribe(Client, {Topic, 1});
-        [_, _, <<"002">>] ->
-            Topic = <<Key/binary, "/v1/ap/#">>,
-            ?LOG(info, "Topic ~p  ", [Topic]),
-            emqtt:subscribe(Client, {Topic, 1});
-        _ ->
-            pass
-    end,
+handle_info({subscribe, _Client}, Dclient) ->
     {noreply, Dclient};
 
 handle_info(disconnect, Dclient) ->
-    dgiot_metrics:dec(dgiot_zeta, <<"mqtt_online">>, 1),
     ?LOG(info, "disconnect ~p ", [Dclient]),
     {noreply, Dclient};
 
 handle_info({publish, #{payload := _Payload} = _Msg}, Dclient) ->
-    dgiot_metrics:inc(dgiot_zeta, <<"mqtt_recv">>, 1),
     {noreply, Dclient};
 
 handle_info(_Info, Dclient) ->
