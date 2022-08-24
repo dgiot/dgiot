a4308406ca0caf92cfa19fbae5b274bb71000c8b
qingn1 <1729727316@qq.com>
2022-08-24 11:03:23 +0800

    磐安工厂2

--- apps/dgiot_http/src/dgiot_http_handler.erl
+++ apps/dgiot_http/src/dgiot_http_handler.erl
@@ -107,7 +107,7 @@ do_request(post_handlewarnsendsms, #{<<"appId">> := AppId, <<"appKey">> := AppKe
     end;
 
 %数字工厂告警
-do_request(post_warnsendsms, #{<<"objectId">> := DeviceId, <<"branchId">> := BranchId, <<"datetimes">> := DateTimes, <<"docnumber">> := Docnumber, <<"username">> := UserName, <<"workshop">> := Workshop, <<"level">> := Level, <<"desc">> := Desc, <<"file">> := FileInfo}, _Context, _Req) ->
+do_request(post_warnsendsms, #{<<"objectId">> := DeviceId, <<"department">> := Department,<<"branchId">> := BranchId,"orderId":= OrderId, <<"datetimes">> := DateTimes, <<"docnumber">> := Docnumber, <<"username">> := UserName, <<"workshop">> := Workshop, <<"level">> := Level, <<"desc">> := Desc, <<"file">> := FileInfo}, _Context, _Req) ->
 
     case Level of
         <<"1">> ->
@@ -131,6 +131,8 @@ do_request(post_warnsendsms, #{<<"objectId">> := DeviceId, <<"branchId">> := Bra
                 <<"content">> => #{
                     <<"alarm">> => #{
                         <<"deviceId"/utf8>> => DeviceId,
+                        <<"orderId"/utf8>> => OrderId,
+                        <<"department"/utf8>> => Department,
                         <<"docnumber"/utf8>> => Docnumber,
                         <<"datetimes"/utf8>> => DateTimes,
                         <<"username"/utf8>> => UserName,
