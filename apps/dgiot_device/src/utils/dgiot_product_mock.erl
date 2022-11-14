f81e7548508d523cb80523a6f520b0ca76e65532
jhonliu <34489690@qq.com>
2022-11-14 14:28:42 +0800

    feat: add dgiot_product_mock

---
+++ apps/dgiot_device/src/utils/dgiot_product_mock.erl
@@ -0,0 +1,68 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2020-2021 DGIOT Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module(dgiot_product_mock).
+-author("jonliu").
+-include("dgiot_device.hrl").
+-include_lib("dgiot/include/logger.hrl").
+-include_lib("dgiot_bridge/include/dgiot_bridge.hrl").
+-dgiot_data("ets").
+-export([init_ets/0, get_data/1]).
+
+
+init_ets() ->
+    dgiot_data:init(?MODULE).
+
+%% 设备类型
+get_data(ProductId) ->
+    case dgiot_product:lookup_prod(ProductId) of
+        {ok, #{<<"thing">> := #{<<"properties">> := Props}}} ->
+            lists:foldl(
+                fun
+                    (#{<<"identifier">> := _Identifier, <<"dataForm">> := #{<<"strategy">> := <<"计算值"/utf8>>}}, Acc) ->
+                        Acc;
+                    (#{<<"identifier">> := Identifier, <<"dataType">> := Datatype}, Acc) ->
+                        Last = dgiot_data:get(?MODULE, {ProductId, mock, Identifier}),
+                        Value = get_value(Last, Datatype),
+                        dgiot_data:insert(?MODULE, {ProductId, mock, Identifier}, Value),
+                        Acc#{Identifier => Value}
+                end, #{}, Props);
+        _Error ->
+            #{}
+    end.
+
+get_value(_Last, #{<<"type">> := <<"INT">>}) ->
+    0;
+
+get_value(_Last, #{<<"dataType">> := #{<<"type">> := <<"INT">>}}) ->
+    0;
+
+get_value(_Last, #{<<"type">> := _Type}) when _Type == <<"FLOAT">>; _Type == <<"DOUBLE">> ->
+    0;
+
+get_value(_Last, #{<<"type">> := _Type}) when _Type == <<"FLOAT">>; _Type == <<"DOUBLE">> ->
+    0;
+
+get_value(_Last, #{<<"type">> := <<"STRING">>}) ->
+    0;
+
+get_value(_Last, #{<<"type">> := <<"BOOL">>}) ->
+    0;
+
+%% @todo 其它类型处理
+get_value(_, _) ->
+    0.
+
