e3cf8d5502dd952800b6ed0f7a5b3f4f6ce80ba2
dawnwinterLiu <1737801684@qq.com>
2022-11-14 19:05:01 +0800

    fix: parse.conf

--- rebar.config.erl
+++ rebar.config.erl
@@ -223,7 +223,6 @@ overlay_vars_rel(RelType) ->
         , {enable_plugin_dgiot_bacnet, true}
         , {enable_plugin_dgiot_factory, true}
         , {enable_plugin_dgiot_printer, true}
-        , {enable_plugin_dgiot_zhcltech, true}
         , {vm_args_file, VmArgs}
     ].
 
@@ -356,7 +355,6 @@ relx_plugin_apps_per_rel(cloud) ->
         , dgiot_bacnet
         , dgiot_factory
         , dgiot_printer
-        , dgiot_zhcltech
     ];
 relx_plugin_apps_per_rel(edge) ->
     [].
