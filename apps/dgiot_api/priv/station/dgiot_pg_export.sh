5658cf714ac7e14d7c83493a026a562a14c37a45
dawnwinterLiu <1737801684@qq.com>
2023-01-13 19:47:58 +0800

    feat: pre-ci

--- apps/dgiot_api/priv/station/dgiot_pg_export.sh
+++ apps/dgiot_api/priv/station/dgiot_pg_export.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 acl=$1
-tables=(_Role Category Channel Device Dict Evidence Files Instruct Maintenance Menu Notification Permission Product ProductTemplet View)
+tables=(_Role _User Category Channel Device Dict Evidence Files Instruct Maintenance Menu Notification Permission Product ProductTemplet View)
 echo ${#tables[@]}
 rm -rf /home/station/postgres
 mkdir -p /home/station/postgres
@@ -9,15 +9,14 @@ chown -R postgres:postgres /home/station/postgres
 
 for i in ${tables[@]}; do
   echo $i
-  psql -U postgres -d parse -c "COPY (SELECT * FROM \"$i\" where _rperm && array['*','role:$1'])  TO '/home/station/postgres/$i.csv' WITH  CSV HEADER;"
+  sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -d parse -c "COPY \"$i\" TO '/home/station/postgres/$i.csv' WITH  CSV HEADER;"
 
 done
 
-relations=(_User _Join:children:Product _Join:deletedBy:Notification _Join:menus:_Role _Join:product:Channel _Join:products:_Role _Join:readBy:Notification _Join:role:_User _Join:roles:_Role _Join:roles:_User _Join:rules:_Role _Join:users:_Role _Join:users:_Session _Join:views:_Role)
+relations=(_Join:children:Product _Join:deletedBy:Notification _Join:menus:_Role _Join:product:Channel _Join:products:_Role _Join:readBy:Notification _Join:role:_User _Join:roles:_Role _Join:roles:_User _Join:rules:_Role _Join:users:_Role _Join:users:_Session _Join:views:_Role)
 mkdir -p /home/station/postgres/relations
 chown -R postgres:postgres /home/station/postgres/relations
 for i in ${relations[@]}; do
   echo $i
-  psql -U postgres -d parse -c "COPY \"$i\" TO '/home/station/postgres/relations/$i.csv' WITH  CSV HEADER;"
-
+  sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -d parse -c "COPY \"$i\" TO '/home/station/postgres/relations/$i.csv' WITH  CSV HEADER;"
 done
