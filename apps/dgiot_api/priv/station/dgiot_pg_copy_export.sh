cfb7a3de463566f1a8ee269c22efc06a86c8aa92
dawnwinterLiu <1737801684@qq.com>
2023-01-16 18:37:32 +0800

    feat: station import

--- apps/dgiot_api/priv/station/dgiot_pg_copy_export.sh
+++ apps/dgiot_api/priv/station/dgiot_pg_copy_export.sh
@@ -9,8 +9,7 @@ chown -R postgres:postgres /home/station/postgres
 
 for i in ${tables[@]}; do
   echo $i
-  sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -d parse -c "COPY \"$i\" TO '/home/station/postgres/$i.csv' WITH  CSV HEADER;"
-
+  sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -d parse -c "COPY \"Dict\" TO '/home/station/postgres/Dict.csv' WITH  CSV HEADER;"
 done
 
 relations=(_Join:children:Product _Join:deletedBy:Notification _Join:menus:_Role _Join:product:Channel _Join:products:_Role _Join:readBy:Notification _Join:role:_User _Join:roles:_Role _Join:roles:_User _Join:rules:_Role _Join:users:_Role _Join:users:_Session _Join:views:_Role)
