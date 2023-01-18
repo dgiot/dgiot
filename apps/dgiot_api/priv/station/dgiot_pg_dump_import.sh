ecf5d14518f24612b961ab01ce560d2d24374000
dawnwinterLiu <1737801684@qq.com>
2023-01-18 15:44:21 +0800

    fix: get_baidu_gps

--- apps/dgiot_api/priv/station/dgiot_pg_dump_import.sh
+++ apps/dgiot_api/priv/station/dgiot_pg_dump_import.sh
@@ -7,6 +7,7 @@ tar -zxvf ${filename}
 cd station/postgres/
 tar -zxvf parse_5.0.sql.tar.gz
 
-sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -c "DROP DATABASE parse;"
+sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname='parse' AND pid<>pg_backend_pid();"
+sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -c "Drop database parse;"
 sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -c "CREATE DATABASE parse;"
 sudo -u postgres /usr/local/pgsql/12/bin/psql -U postgres -f ./parse_5.0.sql  parse
