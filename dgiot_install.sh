cbb32658cc6e70dcc91c17fe10cb4d54655865e0
dawnwinterLiu <1737801684@qq.com>
2023-03-17 16:40:35 +0800

    update 4.7.8

--- dgiot_install.sh
+++ dgiot_install.sh
@@ -1544,13 +1544,13 @@ dgiot_shell
 deployType=single                             # [single | cluster | devops | ci]
 domain_name="prod.dgiotcloud.cn"              # [prod.dgiotcloud.cn | your_domain_name]
 plugin="dgiot"                                # [dgiot | dgiot_your_plugin]
-software="dgiot_n300"                          # [dgiot_n300| dgiot_n]
-dgiotmd5="27335b4c53211463de9105df2f4adf4a"   # [dgiotmd5]
+software="dgiot_b3"                          # [dgiot_b3| dgiot_n]
+dgiotmd5="6c87d58a676855d9a3759b67cc9e50bd"   # [dgiotmd5]
 pg_eip="changeyourip"                            # [datanode_eip]
 pg_auth='changeyourpassword'                  # [pg_auth]
 islanip="false"                                    # [islanip]
-html_software="dgiot_html_4.7.7"                          # [dgiot_html_4.7.7| dgiot_html_n]
-htmlmd5="5afe4baa96517235108dc82153124bc2"   # [htmlmd5]
+html_software="dgiot_html_4.7.8"                          # [dgiot_html_4.7.8| dgiot_html_n]
+htmlmd5="94f02a88f596d21c599587fa34838c2f"   # [htmlmd5]
 
 while getopts "v:s:p:m:d:e:a:n:" arg
 do
