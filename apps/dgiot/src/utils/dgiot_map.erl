cc1f8e0a2605b6a3e315226dc5a1309518cb5d5a
jhonliu <34489690@qq.com>
2023-04-01 15:16:18 +0800

    feat: add dgiot_map list and json

--- apps/dgiot/src/utils/dgiot_map.erl
+++ apps/dgiot/src/utils/dgiot_map.erl
@@ -13,91 +13,12 @@
 -export([unflatten/1, unflatten/2]).
 -export([test/0, map/2]).
 
-unflatten(Data) ->
-    unflatten(Data, "_").
-
-unflatten(List, Link) when is_list(List) ->
-    lists:foldl(
-        fun(X, Acc) ->
-            Acc ++ [unflatten(X, Link)]
-        end, [], List);
-
-unflatten(Map, Link) when is_map(Map) ->
-    maps:fold(
-        fun(L, V, Acc) ->
-            KList = lists:reverse(re:split(L, Link)),
-            NewMap = get_map(KList, V),
-            merge(Acc, NewMap)
-        end, #{}, Map);
-
-unflatten(Data, _) ->
-    Data.
-
-get_map(KList, V) ->
-    lists:foldl(
-        fun(X, Acc) ->
-            Value = case maps:size(Acc) of
-                        0 ->
-                            V;
-                        _ ->
-                            Acc
-                    end,
-            case re:run(X, <<"\[[0-9]*\]">>) of
-                {match, [{First, Len}]} ->
-                    XList = dgiot_utils:to_list(X),
-                    Key = lists:sublist(XList, 1, First),
-                    Index = dgiot_utils:to_int(dgiot_utils:to_binary(lists:sublist(XList, First + 2, Len - 2))),
-                    get_list(Key, Index, Value);
-                _ ->
-                    #{X => Value}
-            end
-        end, #{}, KList).
-
-get_list(Key, Index, Value) when Index > 0 ->
-    Head = lists:foldl(
-        fun(_, Acc) ->
-            Acc ++ [[]]
-        end, [], lists:seq(1, Index)),
-    #{dgiot_utils:to_binary(Key) => Head ++ [Value]};
-get_list(Key, _, Value) ->
-    #{dgiot_utils:to_binary(Key) => [Value]}.
-
-flatten(Map) ->
-    flatten(Map, <<"_">>).
-
-flatten(Map, Link) ->
-    case is_map(Map) of
-        true ->
-            maps:fold(
-                fun(K, V, Acc) ->
-                    maps:merge(Acc, flatten(<<K/binary>>, V, Link))
-                end, #{}, Map);
-        false ->
-            {error, <<"wrong_type">>}
-
-    end.
-
-flatten(Head, Map, Link) ->
-    case is_map(Map) of
-        true ->
-            maps:fold(
-                fun(K, V, Acc) ->
-                    maps:merge(Acc, flatten(<<Head/binary, Link/binary, K/binary>>, V, Link))
-                end, #{}, Map);
-        false ->
-            #{<<Head/binary>> => Map}
-
-    end.
-
-
 merge(Data, NewData) ->
     maps:fold(
         fun(NewKey, NewValue, Acc) ->
             case maps:find(NewKey, Acc) of
                 {ok, Value} when is_map(Value) and is_map(NewValue) ->
                     Acc#{NewKey => merge(Value, NewValue)};
-%%                {ok, Value} when is_list(Value) and is_list(NewValue) ->
-%%                    Acc#{NewKey => lists:merge(Value, NewValue)};
                 _ ->
                     Acc#{NewKey => NewValue}
             end
@@ -154,7 +75,7 @@ value(_, _Value) ->
     undefined.
 
 test_get() ->
-    Keys = [<<"content.i_out">>, <<"content.i_in">>,<<"name.[0].b.[0].c">>],
+    Keys = [<<"content.i_out">>, <<"content.i_in">>, <<"name.[0].b.[0].c">>],
     Data = #{<<"content">> => #{<<"i_out">> => 1, <<"i_in">> => 9},
         <<"name">> => [
             #{<<"b">> => [#{<<"c">> => 9}]}
@@ -166,6 +87,81 @@ test_merge() ->
     A = #{1 => #{1 => 11}},
     B = #{1 => #{1 => 2, 2 => 3}, 2 => #{4 => 5}},
     merge(A, B).
+
+unflatten(Data) ->
+    unflatten(Data, "_").
+
+unflatten(List, Link) when is_list(List) ->
+    lists:foldl(
+        fun(X, Acc) ->
+            Acc ++ [unflatten(X, Link)]
+        end, [], List);
+
+unflatten(Map, Link) when is_map(Map) ->
+    maps:fold(
+        fun(L, V, Acc) ->
+            KList = lists:reverse(re:split(L, Link)),
+            NewMap = get_map(KList, V),
+            merge(Acc, NewMap)
+        end, #{}, Map);
+
+unflatten(Data, _) ->
+    Data.
+
+get_map(KList, V) ->
+    lists:foldl(
+        fun(X, Acc) ->
+            Value =
+                case maps:size(Acc) of
+                    0 -> V;
+                    _ -> Acc
+                end,
+            case re:run(X, <<"\[[0-9]*\]">>) of
+                {match, [{First, Len}]} ->
+                    XList = dgiot_utils:to_list(X),
+                    Key = lists:sublist(XList, 1, First),
+                    Index = dgiot_utils:to_int(dgiot_utils:to_binary(lists:sublist(XList, First + 2, Len - 2))),
+                    get_list(Key, Index, Value);
+                _ ->
+                    #{X => Value}
+            end
+        end, #{}, KList).
+
+get_list(Key, Index, Value) when Index > 0 ->
+    Head = lists:foldl(
+        fun(_, Acc) ->
+            Acc ++ [[]]
+        end, [], lists:seq(1, Index)),
+    #{dgiot_utils:to_binary(Key) => Head ++ [Value]};
+get_list(Key, _, Value) ->
+    #{dgiot_utils:to_binary(Key) => [Value]}.
+
+flatten(Map) ->
+    flatten(Map, <<"_">>).
+
+flatten(Map, Link) ->
+    case is_map(Map) of
+        true ->
+            maps:fold(
+                fun(K, V, Acc) ->
+                    maps:merge(Acc, flatten(<<K/binary>>, V, Link))
+                end, #{}, Map);
+        false ->
+            {error, <<"wrong_type">>}
+
+    end.
+
+flatten(Head, Map, Link) ->
+    case is_map(Map) of
+        true ->
+            maps:fold(
+                fun(K, V, Acc) ->
+                    maps:merge(Acc, flatten(<<Head/binary, Link/binary, K/binary>>, V, Link))
+                end, #{}, Map);
+        false ->
+            #{<<Head/binary>> => Map}
+
+    end.
 
 test() ->
     {file, Here} = code:is_loaded(?MODULE),
@@ -193,7 +189,6 @@ test() ->
 %%{{ test_var }}
 %%
 %%{% include "path2/template2" %}
-
 map(Map, Template) ->
     case erlydtl:compile({template, Template}, dgiot_render, [{out_dir, false}]) of
         {ok, Render} ->
