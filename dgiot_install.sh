1066e8053955e0db05e29acf1ae92ce5909e8b09
QiFeeng <93361221+QiFeeng@users.noreply.github.com>
2023-04-28 17:49:13 +0800

    updata v4.8.1

--- dgiot_install.sh
+++ dgiot_install.sh
@@ -1544,13 +1544,13 @@ dgiot_shell
 deployType=single                             # [single | cluster | devops | ci]
 domain_name="prod.dgiotcloud.cn"              # [prod.dgiotcloud.cn | your_domain_name]
 plugin="dgiot"                                # [dgiot | dgiot_your_plugin]
-software="dgiot_b12"                          # [dgiot_b12| dgiot_n]
-dgiotmd5="311498d97413b616ff6f139c257c3e9e"   # [dgiotmd5]
+software="dgiot_b19"                          # [dgiot_b19| dgiot_n]
+dgiotmd5="f29537f0c55e0836aed07b1db9e60ba9"   # [dgiotmd5]
 pg_eip="changeyourip"                            # [datanode_eip]
 pg_auth='changeyourpassword'                  # [pg_auth]
 islanip="false"                                    # [islanip]
-html_software="dgiot_html_4.8.0"                          # [dgiot_html_4.8.0| dgiot_html_n]
-htmlmd5="2bc27a3409c5e88c77218dc38fb0e23a"   # [htmlmd5]
+html_software="dgiot_html_4.8.1"                          # [dgiot_html_4.8.1| dgiot_html_n]
+htmlmd5="c30919485511cdfb2842e414c5929a87"   # [htmlmd5]
 
 while getopts "v:s:p:m:d:e:a:n:" arg
 do
