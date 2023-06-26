c16ed0332d9b5f7f515f3778fd2274644e624a02
jhonliu <34489690@qq.com>
2023-06-26 17:20:38 +0800

    feat: add n2n

--- rebar.config.erl
+++ rebar.config.erl
@@ -230,6 +230,9 @@ overlay_vars_rel(RelType) ->
         , {enable_plugin_dgiot_factory, true}
         , {enable_plugin_dgiot_printer, true}
         , {enable_plugin_dgiot_location, true}
+        , {enable_plugin_dgiot_edge, true}
+        , {enable_plugin_dgiot_current, true}
+        , {enable_plugin_dgiot_workshop, true}
         , {vm_args_file, VmArgs}
     ].
 
@@ -352,6 +355,9 @@ relx_plugin_apps_per_rel(cloud) ->
         , dgiot_factory
         , dgiot_printer
         , dgiot_location
+        , dgiot_edge
+        , dgiot_current
+        , dgiot_workshop
     ];
 relx_plugin_apps_per_rel(edge) ->
     [].
