4e4b56b85b1585b940052c2bca47002e22630ee1
dawnwinterLiu <1737801684@qq.com>
2024-02-22 10:47:31 +0800

    feat: TASKSTATISTICS

--- apps/dgiot_atomgit/include/dgiot_atomgit.hrl
+++ apps/dgiot_atomgit/include/dgiot_atomgit.hrl
@@ -1,25 +1,127 @@
-%%--------------------------------------------------------------------
-%% Copyright (c) 2020 DGIOT Technologies Co., Ltd. All Rights Reserved.
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License");
-%% you may not use this file except in compliance with the License.
-%% You may obtain a copy of the License at
-%%
-%%     http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS,
-%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-%% See the License for the specific language governing permissions and
-%% limitations under the License.
-%%--------------------------------------------------------------------
-
-
--define(DEMO, <<"DEMO">>).
-
+-define(ATOMGIT, <<"ATOMGIT">>).
 
 -record(state, {
     id,
-    env = #{},
-    search = <<"IP">>
+    devaddr = <<>>,
+    heartcount = 0,
+    regtype = <<>>,
+    head = "xxxxxx0eee",
+    len = 0,
+    app = <<>>,
+    product = <<>>,
+    deviceId = <<>>,
+    scale = 10,
+    temperature = 0,
+    env = <<>>,
+    dtutype = <<>>,
+    hb = 60,
+    maxaddr = 0
 }).
+
+%%设置超时时间及重发次数
+-define(SetTimeOutReSendTimes, 1000).
+%%提取现场机时间
+-define(GetSceneDeviceTime, 1011).
+%%上传现场机时间
+-define(UploadSceneDeviceTime, 1011).
+%%设置现场机时间
+-define(SetSceneDeviceTime, 1012).
+%%现场机时间校准请求
+-define(SceneDeviceTimeCalibration, 1013).
+%%提取实时数据间隔
+-define(GetRtdDataInterval, 1061).
+%%上传实时数据间隔
+-define(UploadRtdDataInterval, 1061).
+%%设置实时数据间隔
+-define(SetRtdDataInterval, 1062).
+%%提取分钟数据间隔
+-define(GetMinuteDataInterval, 1063).
+%%上传分钟数据间隔
+-define(UploadMinuteDataInterval, 1063).
+%%设置分钟数据间隔
+-define(SetMinuteDataInterval, 1064).
+%%用于设置现场机的密码
+-define(SetSceneDevicePassword, 1072).
+%%取污染物实时数据
+-define(GetRtdData, 2011).
+%%上传污染物实时数据
+-define(UploadRtdData, 2011).
+%%停止察看污染物实时数据
+-define(StopRtdData, 2012).
+%%取设备运行状态数据
+-define(GetDeviceRunState, 2021).
+%%上传设备运行状态数据
+-define(UploadDeviceRunState, 2021).
+%%停止察看设备运行状态
+-define(StopDeviceRunState, 2022).
+%%取污染物日历史数据
+-define(GetDayData, 2031).
+%%上传污染物日历史数据
+-define(UploadDayData, 2031).
+%%取设备运行时间日历史数据
+-define(GetDeviceRunTimeDayData, 2041).
+%%上传设备运行时间日历史数据
+-define(UploadDeviceRunTimeDayData, 2041).
+%%取污染物分钟数据
+-define(GetMinuteData, 2051).
+%%上传污染物分钟数据
+-define(UploadMinuteData, 2051).
+%%取污染物小时数据
+-define(GetHourData, 2061).
+%%上传污染物小时数据
+-define(UploadHourData, 2061).
+%%上传数采仪开机时间
+-define(UploadComputerPowerOnTime, 2081).
+%%零点校准量程校准
+-define(RangeCalibration, 3011).
+%%即时采样
+-define(TakeSampleImmediately, 3012).
+%%启动清洗/反吹
+-define(StartClear, 3013).
+%%比对采样
+-define(CompareSample, 3014).
+%%超标留样
+-define(LeaveSuperstandardSample, 3015).
+%%上传超标留样信息
+-define(UploadSuperstandardSample, 3015).
+%%设置采样时间周期
+-define(SetSampleTimeInterval, 3016).
+%%提取采样时间周期
+-define(GetSampleTimeInterval, 3017).
+%%上传采样时间周期
+-define(UploadSampleTimeInterval, 3017).
+%%提取出样时间
+-define(GetSampleTime, 3018).
+%%上传出样时间
+-define(UploadSampleTime, 3018).
+%%提取设备唯一标识
+-define(GetSceneDeviceUUID, 3019).
+%%上传设备唯一标识
+-define(UploadSceneDeviceUUID, 3019).
+%%提取现场机信息
+-define(GetSceneDeviceInfo, 3020).
+%%上传现场机信息
+-define(UploadSceneDeviceInfo, 3020).
+%%设置现场机参数
+-define(SetSceneDeviceParam, 3021).
+%%取污染物周期数据
+-define(GetCycleData, 8051).
+%%上传污染物周期数据
+-define(UploadCycleData, 8051).
+%%请求应答
+-define(RequestResponse, 9011).
+%%执行结果
+-define(ExecuteResponse, 9012).
+%%通知应答
+-define(NoticeResponse, 9013).
+%%数据应答
+-define(DataResponse, 9014).
+%%心跳
+-define(HeartBeat, 9021).
+
+%%commandType
+-define(Request, 1).  %%请求命令
+-define(Upload, 2).  %% 上传命令
+-define(Notice, 3).  %% 通知命令
+-define(Other, 4). %% 其他
+-define(None, 5).  %%不支持
--- apps/dgiot_atomgit/src/dgiot_atomgit_channel.erl
+++ apps/dgiot_atomgit/src/dgiot_atomgit_channel.erl
@@ -21,7 +21,7 @@
 -include_lib("dgiot/include/dgiot_socket.hrl").
 -include_lib("dgiot/include/logger.hrl").
 -include("dgiot_atomgit.hrl").
--define(TYPE, <<"CUSTOMER">>).
+-define(TYPE, <<"ATOMGIT">>).
 %% API
 -export([start/2]).
 
@@ -33,19 +33,31 @@
     cType => ?TYPE,
     type => ?PROTOCOL_CHL,
     title => #{
-        zh => <<"demo测试通道"/utf8>>
+        zh => <<"ATOMGIT测试通道"/utf8>>
     },
     description => #{
-        zh => <<"demo测试通道"/utf8>>
+        zh => <<"ATOMGIT测试通道"/utf8>>
     }
 }).
 %% 注册通道参数
 -params(#{
+    <<"port">> => #{
+        order => 1,
+        type => integer,
+        required => true,
+        default => 20660,
+        title => #{
+            zh => <<"端口"/utf8>>
+        },
+        description => #{
+            zh => <<"侦听端口"/utf8>>
+        }
+    },
     <<"ico">> => #{
         order => 102,
         type => string,
         required => false,
-        default => <<"/dgiot_file/shuwa_tech/zh/product/dgiot/channel/demo.jpg">>,
+        default => <<"/dgiot_file/shuwa_tech/zh/product/dgiot/channel/atomgit.jpg">>,
         title => #{
             en => <<"channel ICO">>,
             zh => <<"通道ICO"/utf8>>
@@ -62,12 +74,26 @@ start(ChannelId, ChannelArgs) ->
     dgiot_channelx:add(?TYPE, ChannelId, ?MODULE, ChannelArgs).
 
 %% 通道初始化
-init(?TYPE, ChannelId, Args) ->
+init(?TYPE, ChannelId, #{
+    <<"port">> := Port,
+    <<"product">> := Products
+} = Args) ->
+    {ProdcutId, App} =
+        case get_app(Products) of
+            [{ProdcutId1, App1} | _] ->
+                {ProdcutId1, App1};
+            [] ->
+                {<<>>, <<>>};
+            _ ->
+                {<<>>, <<>>}
+        end,
     State = #state{
         id = ChannelId,
-        env = Args
+        env = Args,
+        app = App,
+        product = ProdcutId
     },
-    {ok, State, []}.
+    {ok, State, dgiot_atomgit_tcp:start(Port, State)}.
 
 handle_init(State) ->
     {ok, State}.
@@ -77,9 +103,39 @@ handle_event(EventId, Event, State) ->
     ?LOG(error, "EventId ~p Event ~p", [EventId, Event]),
     {ok, State}.
 
+handle_message({dlink_properties_report, ProductId, DevAddr, Buff}, #state{id = ChannelId} = State) ->
+    dgiot_bridge:send_log(ChannelId, ProductId, "~s ~p ~p mqtt revice from ~p => ProductId ~p ", [?FILE, ?LINE, dgiot_datetime:format("YYYY-MM-DD HH:NN:SS"), Buff, ProductId]),
+    case dgiot_atomgit_decoder:parse_frame(Buff, []) of
+        {ok, [#{<<"devaddr">> := DevAddr} = Data | _]} ->
+            NewData = dgiot_dlink_proctol:parse_payload(ProductId, Data),
+            dgiot_bridge:send_log(ChannelId, ProductId, DevAddr, "~s ~p ~p revice from ~p~n save td => ProductId ~p DevAddr ~p ~ts ",
+                [?FILE, ?LINE, dgiot_datetime:format("YYYY-MM-DD HH:NN:SS"), Buff, ProductId, DevAddr, unicode:characters_to_list(dgiot_json:encode(NewData))]),
+            dgiot_task:save_td(ProductId, DevAddr, NewData, #{});
+        _ ->
+            pass
+    end,
+    {ok, State};
+
 handle_message(_Message, State) ->
     {ok, State}.
 
 stop(_ChannelType, _ChannelId, _State) ->
     ok.
 
+get_app(Products) ->
+    lists:map(fun({ProdcutId, #{<<"ACL">> := Acl}}) ->
+        Predicate = fun(E) ->
+            case E of
+                <<"role:", _/binary>> -> true;
+                _ -> false
+            end
+                    end,
+        App =
+            case lists:filter(Predicate, maps:keys(Acl)) of
+                [<<"role:", Name/binary>> | _] ->
+                    Name;
+                _ ->
+                    <<"dgiot">>
+            end,
+        {ProdcutId, App}
+              end, Products).
