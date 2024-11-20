9e4bed1dd9d6d4881280f535d54693c81a9d9024
stoneliu <dgiot@iotn2n.com>
2024-11-20 09:45:26 +0800

    feat: ad

--- apps/dgiot_modbus/src/dgiot_modbus_channel.erl
+++ apps/dgiot_modbus/src/dgiot_modbus_channel.erl
@@ -91,7 +91,7 @@
         order => 102,
         type => string,
         required => false,
-        default => <<"/dgiot_file/shuwa_tech/zh/product/dgiot/channel/modbus.png">>,
+        default => <<"/dgiot_file/shuwa_tech/zh/product/dgiot/channel/modbus_channel.png">>,
         title => #{
             en => <<"channel ICO">>,
             zh => <<"通道ICO"/utf8>>
